2b92cd238a7c1202b1ab083499e43b9ff07cf637
Alessandro Scala <alessandro.scala8@gmail.com>
2024-03-27 21:47:52 +0100

    Extend Regular Commands with heap

--- lib/ast_program.ml
+++ lib/ast_program.ml
@@ -1,87 +1,120 @@
 (* Grammar:
-   RegularCommand ::=  AtomicCommand  |  RegularCommand; RegularCommand  |  RegularCommand + RegularCommand  |  RegularCommand*
-   
-   AtomicCommand ::=  SKIP  |  Identifier = ArithmeticExpression  |  BooleanExpression ?
-   
-   BooleanExpression ::=  TRUE  |  FALSE  |  !BooleanExpression  |  BooleanExpression && BooleanExpression  |  BooleanExpression || BooleanExpression  |  ArithmeticExpression BooleanComparison ArithmeticExpression
-   
-   BooleanComparison ::=  =  |  !=  |  <  |  <=  |  >  |  >=
-   
-   ArithmeticExpression ::=  INT(n)  |  Identifier  |  ArithmeticExpression BinaryOperator ArithmeticExpression
-   
-   BinaryOperator ::=  +  |  -  |  *  |  /  
+	 HeapRegularCommand ::=  HeapAtomicCommand  |  HeapRegularCommand; HeapRegularCommand  |  HeapRegularCommand + HeapRegularCommand  |  HeapRegularCommand*
+	 
+	 HeapAtomicCommand ::=  SKIP  |  Identifier = ArithmeticExpression  |  BooleanExpression ?  |  Identifier = alloc()  |  free(Identifier)  |  x = [y]  |  [x] = ArithmeticExpression
+	 
+	 BooleanExpression ::=  TRUE  |  FALSE  |  !BooleanExpression  |  BooleanExpression && BooleanExpression  |  BooleanExpression || BooleanExpression  |  ArithmeticExpression BooleanComparison ArithmeticExpression
+	 
+	 BooleanComparison ::=  =  |  !=  |  <  |  <=  |  >  |  >=
+	 
+	 ArithmeticExpression ::=  INT(n)  |  Identifier  |  ArithmeticExpression BinaryOperator ArithmeticExpression
+	 
+	 BinaryOperator ::=  +  |  -  |  *  |  /  
 *)
 module type AnnotationType = sig
-  type t
+	type t
 end
 
-module ASTRegularCommands(Annotation: AnnotationType) = struct
-  type t = Annotation.t
-  type identifier = string [@@deriving show]
-  type 'a annotated_node = {node: 'a; annotation: t [@opaque]} [@@deriving show]
-  let addAnnotation (node: 'a) (annotation: t) = {node; annotation}
-  let getAnnotation (node: 'a annotated_node) = node.annotation
-  let removeAnnotation (node: 'a annotated_node) = node.node
+module ASTHeapRegularCommands(Annotation: AnnotationType) = struct
+	type t = Annotation.t
+	type identifier = string [@@deriving show]
+	module IdentifierSet = Set.Make(struct type t = identifier let compare = compare end)
+	type 'a annotated_node = {node: 'a; annotation: t [@opaque]} [@@deriving show]
+	let addAnnotation (node: 'a) (annotation: t) = {node; annotation}
+	let getAnnotation (node: 'a annotated_node) = node.annotation
+	let removeAnnotation (node: 'a annotated_node) = node.node
 
-  module ArithmeticOperation = struct
-    type t =
-      | Plus
-      | Minus
-      | Times
-      | Division
-    [@@deriving show]
-  end
+	module ArithmeticOperation = struct
+		type t =
+			| Plus
+			| Minus
+			| Times
+			| Division
+		[@@deriving show]
+	end
 
-  module BooleanComparison = struct
-    type t =
-      | Equal
-      | NotEqual
-      | LessThan
-      | LessOrEqual
-      | GreaterThan
-      | GreaterOrEqual
-    [@@deriving show]
-  end
+	module BooleanComparison = struct
+		type t =
+			| Equal
+			| NotEqual
+			| LessThan
+			| LessOrEqual
+			| GreaterThan
+			| GreaterOrEqual
+		[@@deriving show]
+	end
 
-  module ArithmeticExpression = struct
-    type t_node =
-      | Literal of int
-      | Variable of identifier
-      | BinaryOperation of ArithmeticOperation.t * t * t
-    and t = t_node annotated_node
-    [@@deriving show]
-  end
+	module ArithmeticExpression = struct
+		type t_node =
+			| Literal of int
+			| Variable of identifier
+			| BinaryOperation of ArithmeticOperation.t * t * t
+		and t = t_node annotated_node
+		[@@deriving show]
+	end
 
-  module BooleanExpression = struct
-    type t_node =
-      | True
-      | False
-      | Not of t
-      | And of t * t
-      | Or of t * t
-      | Comparison of BooleanComparison.t * ArithmeticExpression.t * ArithmeticExpression.t
-    and t = t_node annotated_node
-    [@@deriving show]
-  end
+	module BooleanExpression = struct
+		type t_node =
+			| True
+			| False
+			| Not of t
+			| And of t * t
+			| Or of t * t
+			| Comparison of BooleanComparison.t * ArithmeticExpression.t * ArithmeticExpression.t
+		and t = t_node annotated_node
+		[@@deriving show]
+	end
 
-  module AtomicCommand = struct
-    type t_node =
-      | Skip
-      | Assignment of identifier * ArithmeticExpression.t
-      | Guard of BooleanExpression.t
-    and t = t_node annotated_node
-    [@@deriving show]
-  end
+	module HeapAtomicCommand = struct
+		type t_node =
+			| Skip
+			| Assignment of identifier * ArithmeticExpression.t
+			| Guard of BooleanExpression.t
+			| Allocation of identifier
+			| Free of identifier
+			| ReadHeap of identifier * identifier
+			| WriteHeap of identifier * ArithmeticExpression.t
+		and t = t_node annotated_node
+		[@@deriving show]
 
-  module RegularCommand = struct
-    type t_node =
-      | Command of AtomicCommand.t
-      | Sequence of t * t
-      | NondeterministicChoice of t * t
-      | Star of t
-    and t = t_node annotated_node
-    [@@deriving show]
-  end
+		(* sil-paper Section 6.1 *)
+		let modifiedVariables ae =
+			match (removeAnnotation ae) with
+				| Skip								-> (IdentifierSet.empty)
+				| Assignment(id, _)		-> (IdentifierSet.singleton id)
+				| Guard(_)						-> (IdentifierSet.empty) 
+				| Allocation(id)			-> (IdentifierSet.singleton id)
+				| Free(_)							-> (IdentifierSet.empty)
+				| ReadHeap(id, _)			-> (IdentifierSet.singleton id)
+				| WriteHeap(_, _)			-> (IdentifierSet.empty)
+	end
 
-  let show = RegularCommand.show
+	module HeapRegularCommand = struct
+		type t_node =
+			| Command of HeapAtomicCommand.t
+			| Sequence of t * t
+			| NondeterministicChoice of t * t
+			| Star of t
+		and t = t_node annotated_node
+		[@@deriving show]
+
+		(* sil-paper Section 6.1 *)
+		let rec modifiedVariables exp = 
+			match (removeAnnotation exp) with
+				| Command(ac) -> (
+						HeapAtomicCommand.modifiedVariables ac
+					)
+				| NondeterministicChoice(rc1, rc2) -> (
+						IdentifierSet.union (modifiedVariables rc1) (modifiedVariables rc2)
+					)
+				| Sequence(rc1, rc2) -> (
+						IdentifierSet.union (modifiedVariables rc1) (modifiedVariables rc2)
+					)
+				| Star(rc) -> (
+						modifiedVariables rc
+					)
+	end
+
+	let show = HeapRegularCommand.show
+	let modifiedVariables = HeapRegularCommand.modifiedVariables
 end
--- test/test_ast_program.ml
+++ test/test_ast_program.ml
@@ -1,50 +1,50 @@
 open Lisproject.Ast_program
 
 (* Instantiate the AST with the annotation type *)
-module ASTRC = ASTRegularCommands(struct
+module ASTHRC = ASTHeapRegularCommands(struct
   type t = int (* int annotations *)
 end)
 
 let counter = ref 0
 
 (* Annotate a node with a unique integer *)
-let annotate (node: 'a): 'a ASTRC.annotated_node =
-  let out: 'a ASTRC.annotated_node = ASTRC.addAnnotation node !counter in
+let annotate (node: 'a): 'a ASTHRC.annotated_node =
+  let out: 'a ASTHRC.annotated_node = ASTHRC.addAnnotation node !counter in
   counter := !counter + 1;
   out
 
 let () =
   (* Create an AST corresponding to the RegCmd:  x = 1; (x < 10?; x = x + 1)*; !(x < 10)?
      This encodes the command:  x = 1; while(x<10){x = x + 1}  *)
-  let root = annotate (ASTRC.RegularCommand.Sequence(
-    annotate (ASTRC.RegularCommand.Command(annotate (ASTRC.AtomicCommand.Assignment("x", annotate (ASTRC.ArithmeticExpression.Literal 1))))),
+  let root = annotate (ASTHRC.HeapRegularCommand.Sequence(
+    annotate (ASTHRC.HeapRegularCommand.Command(annotate (ASTHRC.HeapAtomicCommand.Assignment("x", annotate (ASTHRC.ArithmeticExpression.Literal 1))))),
 
-    annotate (ASTRC.RegularCommand.Sequence(
-      annotate (ASTRC.RegularCommand.Star(
-        annotate (ASTRC.RegularCommand.Sequence(
-          annotate (ASTRC.RegularCommand.Command(annotate (ASTRC.AtomicCommand.Guard(annotate (ASTRC.BooleanExpression.Comparison(
-            ASTRC.BooleanComparison.LessThan,
-            annotate (ASTRC.ArithmeticExpression.Variable "x"),
-            annotate (ASTRC.ArithmeticExpression.Literal 10)
+    annotate (ASTHRC.HeapRegularCommand.Sequence(
+      annotate (ASTHRC.HeapRegularCommand.Star(
+        annotate (ASTHRC.HeapRegularCommand.Sequence(
+          annotate (ASTHRC.HeapRegularCommand.Command(annotate (ASTHRC.HeapAtomicCommand.Guard(annotate (ASTHRC.BooleanExpression.Comparison(
+            ASTHRC.BooleanComparison.LessThan,
+            annotate (ASTHRC.ArithmeticExpression.Variable "x"),
+            annotate (ASTHRC.ArithmeticExpression.Literal 10)
           )))))),
-          annotate (ASTRC.RegularCommand.Command(annotate (ASTRC.AtomicCommand.Assignment("x", annotate (
-            ASTRC.ArithmeticExpression.BinaryOperation(
-              ASTRC.ArithmeticOperation.Plus,
-              annotate (ASTRC.ArithmeticExpression.Variable "x"),
-              annotate (ASTRC.ArithmeticExpression.Literal 1)))))))
+          annotate (ASTHRC.HeapRegularCommand.Command(annotate (ASTHRC.HeapAtomicCommand.Assignment("x", annotate (
+            ASTHRC.ArithmeticExpression.BinaryOperation(
+              ASTHRC.ArithmeticOperation.Plus,
+              annotate (ASTHRC.ArithmeticExpression.Variable "x"),
+              annotate (ASTHRC.ArithmeticExpression.Literal 1)))))))
         ))
       )),
 
-      annotate (ASTRC.RegularCommand.Command(annotate (ASTRC.AtomicCommand.Guard(annotate (ASTRC.BooleanExpression.Not(annotate (ASTRC.BooleanExpression.Comparison(
-        ASTRC.BooleanComparison.LessThan,
-        annotate (ASTRC.ArithmeticExpression.Variable "x"),
-        annotate (ASTRC.ArithmeticExpression.Literal 10)
+      annotate (ASTHRC.HeapRegularCommand.Command(annotate (ASTHRC.HeapAtomicCommand.Guard(annotate (ASTHRC.BooleanExpression.Not(annotate (ASTHRC.BooleanExpression.Comparison(
+        ASTHRC.BooleanComparison.LessThan,
+        annotate (ASTHRC.ArithmeticExpression.Variable "x"),
+        annotate (ASTHRC.ArithmeticExpression.Literal 10)
       ))))))))
     ))
   )) in
-  match ASTRC.removeAnnotation root with
-    | Command(a) -> (match ASTRC.removeAnnotation a with
-      | Guard(a) -> (match ASTRC.removeAnnotation a with
+  match ASTHRC.removeAnnotation root with
+    | Command(a) -> (match ASTHRC.removeAnnotation a with
+      | Guard(a) -> (match ASTHRC.removeAnnotation a with
         | Comparison(a, b, c) -> (match (a, b.node, c.node) with
           | (Equal, Variable(x), Literal(y)) -> print_endline ("Equal: " ^ x ^ " = " ^ (string_of_int y))
           | _ -> print_endline "Not Equal")
@@ -52,5 +52,16 @@ let () =
       | _ -> print_endline "Not Seq")
     | _ -> print_endline "Not Star";
 
-  (* Print it with ASTRC.show_rcmd *)
-  print_endline (ASTRC.show root)
+  (* Print it with ASTHRC.show_rcmd *)
+  print_endline (ASTHRC.show root);
+
+  let root = annotate (ASTHRC.HeapRegularCommand.Sequence(
+    (annotate (ASTHRC.HeapRegularCommand.Command(annotate (ASTHRC.HeapAtomicCommand.Allocation("x"))))),
+    (annotate (ASTHRC.HeapRegularCommand.Sequence (
+    (annotate (ASTHRC.HeapRegularCommand.Command (annotate(ASTHRC.HeapAtomicCommand.WriteHeap("x", annotate (ASTHRC.ArithmeticExpression.Literal 1)))))),
+    (annotate (ASTHRC.HeapRegularCommand.Command (annotate(ASTHRC.HeapAtomicCommand.ReadHeap("y", "x")))))
+    ))
+    )
+  )) in
+  print_endline (ASTHRC.show root);
+  print_endline (ASTHRC.modifiedVariables root |> ASTHRC.IdentifierSet.elements |> String.concat ", ");
