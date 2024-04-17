03905e549c64519c2c9cb22fc1aa052692b04568
Yuri Andriaccio <yurand2000@gmail.com>
2024-04-17 12:25:11 +0200

    Add concrete implementations of the commands and logic data structures, Update documentation for regular commands, PHONY makefile

--- lib/prelude.ml
+++ lib/prelude.ml
@@ -1,3 +1,42 @@
 module Ast = struct
-  
+  type position = {line: int; column: int} [@@deriving show]
+
+  let make_position (line: int) (column: int) = {line; column}
+
+  type logic_formulas_annotation = {
+    position: position;
+  }
+  [@@deriving show]
+
+  module LogicFormulas = struct
+    include Ast.AnnotationLogic(struct
+      type t = logic_formulas_annotation
+    end)
+
+    let annotate formula line column =
+      let make_annotation line column : AnnotatedNode.annotation =
+        let position = make_position line column in
+        {position}
+      in
+      AnnotatedNode.make formula (make_annotation line column)
+  end
+
+  type regular_formulas_annotation = {
+    position: position;
+    logic_formula: LogicFormulas.t option
+  }
+  [@@deriving show]
+
+  module Commands = struct
+    include Ast.HeapRegularCommands(struct
+      type t = regular_formulas_annotation
+    end)
+
+    let annotate command line column formula =
+      let make_annotation line column formula : AnnotatedNode.annotation =
+        let position = make_position line column in
+        {position; logic_formula = formula}
+      in
+      AnnotatedNode.make command (make_annotation line column formula)
+  end
 end
