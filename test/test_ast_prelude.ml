be0a37250b19e825433b87193aea392d514f570f
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-04-30 17:49:17 +0200

    fix test

--- test/test_ast_prelude.ml
+++ test/test_ast_prelude.ml
@@ -1,20 +1,22 @@
 open Lisproject.Prelude.Ast
 
+let pos = make_position 0 1
+
 let () =
   let formula =
-    LogicFormulas.annotate (LogicFormulas.Formula.NonAllocated("x")) 0 1
+    LogicFormulas.annotate (LogicFormulas.Formula.NonAllocated("x")) pos
   in
   let free_x =
     Commands.annotate (Commands.HeapRegularCommand.Command(
-      Commands.annotate (Commands.HeapAtomicCommand.Free "x") 0 1 (Option.Some(formula))
-    )) 0 1 Option.None
+      Commands.annotate (Commands.HeapAtomicCommand.Free "x") pos (Option.Some(formula))
+    )) pos Option.None
   in
   let assing_y =
     Commands.annotate (Commands.HeapRegularCommand.Command(
       Commands.annotate (Commands.HeapAtomicCommand.Assignment("x",
-        Commands.annotate (Commands.ArithmeticExpression.Literal(50)) 0 1 Option.None
-      )) 0 1 Option.None
-    )) 0 1 Option.None
+        Commands.annotate (Commands.ArithmeticExpression.Literal(50)) pos Option.None
+      )) pos Option.None
+    )) pos Option.None
   in
-  let test_ast = Commands.annotate (Commands.HeapRegularCommand.Sequence(free_x, assing_y)) 0 1 Option.None in
+  let test_ast = Commands.annotate (Commands.HeapRegularCommand.Sequence(free_x, assing_y)) pos Option.None in
   print_endline (Commands.show test_ast)
