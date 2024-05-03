6bffb1e2bf7c9bc0754c71af98dc20f072d4b256
Yuri Andriaccio <yurand2000@gmail.com>
2024-05-03 12:26:20 +0200

    Add normal form logic formulas, Implement prodecure to convert formula to normal form

---
+++ lib/analysis/normal_form.ml
@@ -0,0 +1,161 @@
+open Prelude
+open Prelude.Ast.LogicFormulas
+open Utils
+
+module IdentifierSet = struct include Ast.IdentifierSet end
+type annotation = Ast.logic_formulas_annotation
+type identifier = Ast.identifier
+
+type normal_form = {
+  variables: IdentifierSet.t;
+  disjoints: Formula.t list;
+  annotation: annotation;
+  last_phantom_id: int;
+}
+
+let make_normal_form variables disjoints annotation phantom_id =
+  {variables; disjoints; annotation; last_phantom_id = phantom_id}
+
+let rename_variable_in_normal_formula (formula: normal_form) (var: identifier) (new_name: identifier) =
+  let variables = rename_variable_in_set formula.variables var new_name in
+  let disjoints = List.map (function x -> rename_variable_in_formula x var new_name) formula.disjoints in
+  make_normal_form variables disjoints formula.annotation formula.last_phantom_id
+
+let rec existential_disjuntive_normal_form (formula: Formula.t) (last_phantom_id: int) =
+  let new_variable_name (old_var: identifier) (phantom_id: int) =
+    let substr = String.split_on_char '$' old_var in
+    if List.length substr > 2 then
+      raise (Failure "Found more than two $ characters in a variable name")
+    else if List.length substr == 2 then
+      let var_name = List.nth substr 1 in
+      ((string_of_int phantom_id) ^ "$" ^ var_name, phantom_id + 1)
+    else
+      let var_name = List.hd substr in
+      ((string_of_int phantom_id) ^ "$" ^ var_name, phantom_id + 1)
+  in
+  let first_annotation (annot1: annotation) (annot2: annotation) =
+    if annot1.position.line < annot2.position.line then
+      annot1
+    else if annot1.position.line > annot2.position.line then
+      annot2
+    else if annot1.position.column <= annot2.position.column then
+      annot1
+    else
+      annot2
+  in
+  let rec expr_identifiers (expr: ArithmeticExpression.t) =
+    match expr.node with
+    | Literal(_) -> (IdentifierSet.empty)
+    | Variable(id) -> (IdentifierSet.singleton id)
+    | Operation(_, lexpr, rexpr) -> 
+      let l_ids = expr_identifiers lexpr in
+      let r_ids = expr_identifiers rexpr in
+      IdentifierSet.union l_ids r_ids
+  in
+  let rec formula_identifiers (formula: Formula.t) =
+    match formula.node with
+    | True | False | EmptyHeap ->
+      IdentifierSet.empty
+    | Allocation(id, _) | NonAllocated(id) ->
+      IdentifierSet.singleton id
+    | Comparison(_, lexpr, rexpr) -> 
+      IdentifierSet.union (expr_identifiers lexpr) (expr_identifiers rexpr)
+    | And(lformula, rformula) | AndSeparately(lformula, rformula) ->
+      IdentifierSet.union (formula_identifiers lformula) (formula_identifiers rformula)      
+    | Exists(_, _) ->
+      raise (Failure "Formulas of existential abstraction cannot be contained in normal form disjoints")
+    | Or(_, _) ->
+      raise (Failure "Disjunction of formulas cannot be contained in normal form disjoints")
+  in
+  let normal_form_free_variables (formula: normal_form) =
+    let ids = List.map formula_identifiers formula.disjoints in
+    let ids = List.fold_left (fun acc ids -> IdentifierSet.union acc ids) IdentifierSet.empty ids in
+    IdentifierSet.diff ids formula.variables
+  in
+  let rename_variable_in_disjoints (var: identifier) (disjoints: Formula.t list) (phantom_id: int) =
+    let (new_var, phantom_id) = new_variable_name var phantom_id in
+    let disjoints = List.map (fun x -> rename_variable_in_formula x var new_var) disjoints in
+    (disjoints, phantom_id)
+  in
+  let common_free_variables (lformula: normal_form) (rformula: normal_form) =
+    let lformula_free = normal_form_free_variables lformula in
+    let rformula_free = normal_form_free_variables rformula in
+    IdentifierSet.inter lformula_free rformula_free
+  in
+  let rename_common_free_variables (lformula: normal_form) (rformula: normal_form) (last_phantom_id: int) =
+    (* find the set of common free variables, to be renamed them in one of the subformulas *)
+    let common_free_variables = common_free_variables lformula rformula in 
+
+    (* rename the common free variables in the rsubformulas *)
+    let (rformula_disjoints, last_phantom_id) =
+      IdentifierSet.fold (fun elem (disjoints, phantom_id) -> rename_variable_in_disjoints elem disjoints phantom_id)
+      common_free_variables (rformula.disjoints, last_phantom_id)
+    in
+    let rformula = make_normal_form rformula.variables rformula_disjoints rformula.annotation last_phantom_id in
+    (lformula, rformula, last_phantom_id)
+  in
+
+  let handle_and (lformula: normal_form) (rformula: normal_form) (last_phantom_id: int) =
+    let (lformula, rformula, last_phantom_id) = rename_common_free_variables lformula rformula last_phantom_id in
+    let free_variables = common_free_variables lformula rformula in
+    let annotation = first_annotation lformula.annotation rformula.annotation in
+    let cartesian = list_cartesian lformula.disjoints rformula.disjoints in
+    let disjoints = List.map (fun (l, r) -> annotate (Formula.And(l, r)) annotation) cartesian in
+    make_normal_form free_variables disjoints annotation last_phantom_id
+  in
+  let handle_and_separately (lformula: normal_form) (rformula: normal_form) (last_phantom_id: int) =
+    let (lformula, rformula, last_phantom_id) = rename_common_free_variables lformula rformula last_phantom_id in
+    let free_variables = common_free_variables lformula rformula in
+    let annotation = first_annotation lformula.annotation rformula.annotation in
+    let cartesian = list_cartesian lformula.disjoints rformula.disjoints in
+    let disjoints = List.map (fun (l, r) -> annotate (Formula.AndSeparately(l, r)) annotation) cartesian in
+    make_normal_form free_variables disjoints annotation last_phantom_id
+  in
+  let handle_exist (exist_id: identifier) (subformula: normal_form) (exist_annot: annotation) =
+    let variables = subformula.variables in
+    let disjoints = subformula.disjoints in
+    let annotation = first_annotation exist_annot subformula.annotation in
+    let phantom_id = subformula.last_phantom_id in
+    
+    match IdentifierSet.find_opt exist_id subformula.variables with
+    | Some(_) ->
+      (* if the given identifier has already been existentialized, we don't add
+         it to the free variables as it cannot occur in the subformulas *)
+      make_normal_form variables disjoints annotation phantom_id
+    | None ->
+      (* if the existentialized variable does not occur in the subformula disjoints, we don't need to add it *)
+      let free_variables = normal_form_free_variables subformula in 
+      match IdentifierSet.find_opt exist_id free_variables with
+      | Some(_) -> make_normal_form (IdentifierSet.add exist_id variables) disjoints annotation phantom_id
+      | None -> make_normal_form variables disjoints annotation phantom_id
+  in
+  let handle_or (lformula: normal_form) (rformula: normal_form) (last_phantom_id: int) =
+    let (lformula, rformula, last_phantom_id) = rename_common_free_variables lformula rformula last_phantom_id in
+    let free_variables = common_free_variables lformula rformula in
+    let annotation = first_annotation lformula.annotation rformula.annotation in
+    make_normal_form free_variables (lformula.disjoints @ rformula.disjoints) annotation last_phantom_id
+  in
+
+  match formula.node with
+  | True | False | EmptyHeap | NonAllocated(_)
+  | Comparison(_, _, _) | Allocation(_, _) ->
+    make_normal_form IdentifierSet.empty [formula] formula.annotation last_phantom_id
+  | And(lformula, rformula) ->
+    (* normalize recursively *)
+    let lformula = existential_disjuntive_normal_form lformula last_phantom_id in
+    let rformula = existential_disjuntive_normal_form rformula lformula.last_phantom_id in
+    handle_and lformula rformula rformula.last_phantom_id
+  | AndSeparately(lformula, rformula) ->
+    (* normalize recursively *)
+    let lformula = existential_disjuntive_normal_form lformula last_phantom_id in
+    let rformula = existential_disjuntive_normal_form rformula lformula.last_phantom_id in
+    handle_and_separately lformula rformula rformula.last_phantom_id
+  | Exists(id, subformula) ->
+    (* normalize recursively *)
+    let subformula = existential_disjuntive_normal_form subformula last_phantom_id in
+    handle_exist id subformula formula.annotation
+  | Or(lformula, rformula) ->
+    (* normalize recursively *)
+    let lformula = existential_disjuntive_normal_form lformula last_phantom_id in
+    let rformula = existential_disjuntive_normal_form rformula lformula.last_phantom_id in
+    handle_or lformula rformula rformula.last_phantom_id
---
+++ lib/analysis/utils.ml
@@ -0,0 +1,55 @@
+open Prelude
+open Prelude.Ast.LogicFormulas
+
+module IdentifierSet = struct include Ast.IdentifierSet end
+type annotation = Ast.logic_formulas_annotation
+type identifier = Ast.identifier
+
+(* https://stackoverflow.com/a/10893700 *)
+let list_cartesian l l' = 
+  List.concat (List.map (fun e -> List.map (fun e' -> (e,e')) l') l)
+
+let rename_variable_in_set (variables: IdentifierSet.t) (var: identifier) (new_name: identifier) =
+  match IdentifierSet.find_opt var variables with
+  | Some(_) -> IdentifierSet.add new_name (IdentifierSet.remove var variables)
+  | None -> variables
+
+let rec rename_variable_in_formula (disjoint: Formula.t) (var: identifier) (new_name: identifier) =
+  let rename_variable_name (var: identifier) (old_name: identifier) (new_name: identifier) =
+    if var == old_name then new_name else var
+  in
+  let rec rename_variable_in_expression (expr: ArithmeticExpression.t) (var: identifier) (new_name: identifier) =
+    match expr.node with
+    | Literal(_) -> expr
+    | Variable(id) -> update_formula expr (ArithmeticExpression.Variable(rename_variable_name id var new_name))
+    | Operation(op, lexpr, rexpr) -> 
+      let lexpr = rename_variable_in_expression lexpr var new_name in
+      let rexpr = rename_variable_in_expression rexpr var new_name in
+      update_formula disjoint (ArithmeticExpression.Operation(op, lexpr, rexpr))
+  in
+
+  match disjoint.node with
+  | True | False | EmptyHeap ->
+    disjoint
+  | NonAllocated(id) ->
+    update_formula disjoint (Formula.NonAllocated(rename_variable_name id var new_name))
+  | Exists(_, _) ->
+    raise (Failure "Formulas of existential abstraction cannot be contained in normal form disjoints")
+  | And(lformula, rformula) -> 
+    let lformula = rename_variable_in_formula lformula var new_name in
+    let rformula = rename_variable_in_formula rformula var new_name in
+    update_formula disjoint (Formula.And(lformula, rformula))
+  | Or(_, _) ->
+    raise (Failure "Disjunction of formulas cannot be contained in normal form disjoints")
+  | Comparison(op, lexpr, rexpr) ->
+    let lexpr = rename_variable_in_expression lexpr var new_name in
+    let rexpr = rename_variable_in_expression rexpr var new_name in
+    update_formula disjoint (Formula.Comparison(op, lexpr, rexpr))
+  | Allocation(id, expr) -> 
+    let id = rename_variable_name id var new_name in
+    let expr = rename_variable_in_expression expr var new_name in
+    update_formula disjoint (Formula.Allocation(id, expr))
+  | AndSeparately(lformula, rformula) ->
+    let lformula = rename_variable_in_formula lformula var new_name in
+    let rformula = rename_variable_in_formula rformula var new_name in
+    update_formula disjoint (Formula.AndSeparately(lformula, rformula))
