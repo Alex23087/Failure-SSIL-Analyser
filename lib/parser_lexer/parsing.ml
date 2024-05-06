769ad834781623340c7baaecfbaafb70c9014e4d
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-05-06 15:17:11 +0200

    add comments

--- lib/parser_lexer/parsing.ml
+++ lib/parser_lexer/parsing.ml
@@ -1,11 +1,13 @@
 (** parsing utilities *)
   exception Syntax_error of Location.lexeme_pos * string
 
-  let parse _lexer _lexbuf =
-    try Parser.program _lexer _lexbuf with
-    | Lexer.Lexing_error _ as sle ->
-        raise sle
-    | Syntax_error _ as spe ->
-        raise spe
-    | Parser.Error ->
-        raise (Syntax_error (Location.to_lexeme_position _lexbuf, "Syntax error"))
+(** tries to parse what's inside lexbuf using lexer as a scanner; *)
+let parse lexer lexbuf =
+  try Parser.program lexer lexbuf with
+  | Lexer.Lexing_error _ as sle ->
+      raise sle
+  | Syntax_error _ as spe ->
+      raise spe
+    (* unfortunately, a parse error has no info with it *)
+  | Parser.Error ->
+      raise (Syntax_error (Location.to_lexeme_position lexbuf, "Syntax error"))
