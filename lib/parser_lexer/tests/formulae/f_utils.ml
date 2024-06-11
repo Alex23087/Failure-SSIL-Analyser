912e70b42a03f1905d075bec9a285c7b2ee7121a
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-06-11 16:46:57 +0200

    remove duplicated definition

--- lib/parser_lexer/tests/formulae/f_utils.ml
+++ lib/parser_lexer/tests/formulae/f_utils.ml
@@ -1,7 +1,6 @@
 open Prelude.Ast.LogicFormulas
 open Prelude.Ast.LogicFormulas.AnnotatedNode
-
-let dummy_position = { Prelude.Ast.line = -1; column = -1 }
+open Utils
 
 let test_node (formula) =
   {node = formula; annotation = {position = dummy_position}}
