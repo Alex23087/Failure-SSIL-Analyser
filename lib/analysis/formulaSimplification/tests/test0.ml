3b2c18c2c36896f52154b05083052208a7f788fc
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-06-12 20:22:41 +0200

    add test

--- lib/analysis/formulaSimplification/tests/test0.ml
+++ lib/analysis/formulaSimplification/tests/test0.ml
@@ -1,13 +1,20 @@
-open DataStructures
 open DataStructures.Analysis
 open NormalForm
-open Sexplib.Std
 open Base
 
-let%test_unit "test and false simplification" =
+let%test_unit "test 'p && false = false' simplification" =
   let formula = NormalForm.make_from_formula (
-    Formula.And(Formula.False, Formula.Allocation("x", ArithmeticExpression.Literal(1))))
-  in
+    Formula.And(Formula.False, Formula.Allocation("x", ArithmeticExpression.Literal(1)))
+  ) in
   let simplified_formula = FormulaSimplificationBase.simplify_formula formula in
   let expected = NormalForm.make_from_formula Formula.False in
+  [%test_eq: Formula.t list] simplified_formula.disjoints expected.disjoints
+
+let%test_unit "test 'p && true = p' simplification" =
+  let p = Formula.Allocation("x", ArithmeticExpression.Literal(1)) in
+  let formula = NormalForm.make_from_formula (
+    Formula.And(Formula.True, p)
+  ) in
+  let simplified_formula = FormulaSimplificationBase.simplify_formula formula in
+  let expected = NormalForm.make_from_formula p in
   [%test_eq: Formula.t list] simplified_formula.disjoints expected.disjoints
