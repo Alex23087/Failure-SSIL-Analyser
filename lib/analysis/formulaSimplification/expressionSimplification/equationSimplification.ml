895100b4e21761f28deb8aa27d1e866c4b46fe9b
Yuri Andriaccio <yurand2000@gmail.com>
2024-06-17 22:18:33 +0200

    Add smarter equation semplification
    Equations containing no variables can be fully resolved to either True or False

--- lib/analysis/formulaSimplification/expressionSimplification/equationSimplification.ml
+++ lib/analysis/formulaSimplification/expressionSimplification/equationSimplification.ml
@@ -9,13 +9,19 @@ let equation_simplification (formula: NormalForm.t) =
     | Comparison(op, lexpr, rexpr) -> (
       match op with
       | Equals
-      | NotEquals ->
+      | NotEquals -> (
         let any_var = IdentifierSet.choose_opt (Analysis_Utils.get_normal_form_disjoint_identifiers formula) in
         if Option.is_some any_var then
           let lexpr, rexpr = simplify_equation lexpr rexpr (Option.get any_var) in
           Formula.Comparison(op, lexpr, rexpr)
         else
-          formula
+          let expr = ArithmeticExpression.Operation(BinaryOperator.Minus, lexpr, rexpr) in
+          let expr = simplify_expression expr in
+          match expr, op with 
+          | Literal(value), Equals -> if value = 0 then Formula.True else Formula.False
+          | Literal(value), NotEquals -> if value <> 0 then Formula.True else Formula.False
+          | _ -> failwith "unexpected"
+      )
       | _ -> formula
     )
     | _ -> formula
