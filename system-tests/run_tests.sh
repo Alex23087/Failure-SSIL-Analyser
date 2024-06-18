148919bca289bebfdae8440243eed83ccfb969ba
Yuri Andriaccio <yurand2000@gmail.com>
2024-06-18 09:18:19 +0200

    Update function pretty printing, Add script to run tests in batch, Update Makefile to include license on install

---
+++ system-tests/run_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CMD="_install/main.exe"
+TEST_DIR="system-tests"
+TMP_FILE="/tmp/.test-results"
+
+runtest () {
+    $CMD $1 | diff - $2 &> $TMP_FILE
+    if [[ $? != 0 ]]; then
+        echo "*** Test failed: $1";
+        cat $TMP_FILE;
+    fi
+}
+
+TEST_DIR="system-tests"
+for i in {0..3}; do
+    INPUT_FILE="$TEST_DIR/input$i.txt"
+    OUTPUT_FILE="$TEST_DIR/output$i.txt"
+    runtest $INPUT_FILE $OUTPUT_FILE
+done
+
+TEST_DIR="test/Programs"
+for i in {0..29}; do
+    INPUT_FILE="$TEST_DIR/pt${i}_input.txt"
+    OUTPUT_FILE="$TEST_DIR/pt${i}_output.txt"
+    runtest $INPUT_FILE $OUTPUT_FILE
+done
+
