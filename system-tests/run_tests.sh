f20400da5086560d9f7ffe21cea5baa68a5acd63
Yuri Andriaccio <yurand2000@gmail.com>
2024-06-19 02:07:10 +0200

    Update test runner, fix some outputs

--- system-tests/run_tests.sh
+++ system-tests/run_tests.sh
@@ -5,7 +5,8 @@ TEST_DIR="system-tests"
 TMP_FILE="/tmp/.test-results"
 
 runtest () {
-    $CMD $1 | diff - $2 &> $TMP_FILE
+    # diff weirdness -> https://unix.stackexchange.com/a/423188
+    $CMD $1 | diff <(grep -vE '^\s*$' -)  <(grep -vE '^\s*$' $2) &> $TMP_FILE
     if [[ $? != 0 ]]; then
         echo "*** Test failed: $1";
         cat $TMP_FILE;
@@ -13,7 +14,7 @@ runtest () {
 }
 
 TEST_DIR="system-tests"
-for i in {0..29}; do
+for i in {0..28}; do
     INPUT_FILE="$TEST_DIR/pt${i}_input.txt"
     OUTPUT_FILE="$TEST_DIR/pt${i}_output.txt"
     runtest $INPUT_FILE $OUTPUT_FILE
