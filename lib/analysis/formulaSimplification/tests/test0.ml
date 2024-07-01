1ecb1dc5855bd5d9195f1a37cd85ba6c3ee10f25
Yuri Andriaccio <yurand2000@gmail.com>
2024-07-01 11:27:25 +0200

    Add tests for UnitAndSep semplification

--- lib/analysis/formulaSimplification/tests/test0.ml
+++ lib/analysis/formulaSimplification/tests/test0.ml
@@ -10,15 +10,6 @@ let%test_unit "test 'p && false = false' simplification" =
   let expected = NormalForm.make_from_formula Formula.False in
   [%test_eq: Formula.t list] simplified_formula.disjoints expected.disjoints
 
-let%test_unit "test 'p * emp = p' simplification" =
-  let p = Formula.Allocation("x", ArithmeticExpression.Literal(1)) in
-  let formula = NormalForm.make_from_formula (
-    Formula.AndSeparately(p, Formula.EmptyHeap)
-  ) in
-  let simplified_formula = FormulaSimplificationBase.simplify_formula formula in
-  let expected = NormalForm.make_from_formula p in
-  [%test_eq: Formula.t list] simplified_formula.disjoints expected.disjoints
-
 let%test_unit "test 'p || false = p' simplification" =
   let p = Formula.Allocation("x", ArithmeticExpression.Literal(1)) in
   let formula = NormalForm.make DataStructures.IdentifierSet.empty [p; Formula.False] {first_id = 0; last_id = 0} in
